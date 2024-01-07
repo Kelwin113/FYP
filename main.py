25633f1e642957f436316e357a80d57aa691d854
Kelwin Chew <85118254+Kelwin113@users.noreply.github.com>
2024-01-08 01:28:25 +0800

    change

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 
 # Main App
-st.set_page_config(page_title="TasteBuddy", page_icon="Resources/Icon.ico")
-st.sidebar.image("Resources/Logo3.png", use_column_width=True)
+# st.set_page_config(page_title="TasteBuddy", page_icon="Resources/Icon.ico")
+st.sidebar.image("Logo3.png", use_column_width=True)
 page_options = ["Home Page","User Profile","Algorithm","Hybrid Food Recommender"]
 page_selection = st.sidebar.selectbox("Choose Option", page_options)
 
