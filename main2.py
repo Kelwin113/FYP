abffe416b84e8aeeee9b82c3da0f3578a68d9b31
Kelwin Chew <85118254+Kelwin113@users.noreply.github.com>
2024-01-08 22:47:15 +0800

    change

--- main2.py
+++ main2.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import re
 
 # Main App
 st.set_page_config(page_title="TasteBuddy", page_icon="Resources/Icon.ico")
@@ -11,7 +10,7 @@ page_options = ["Home Page","User Profile","Algorithm","Hybrid Food Recommender"
 page_selection = st.sidebar.selectbox("Choose Option", page_options)
 
 # Custom Libraries
-from Backend.model import model1, model2, final_model, get_users, get_name, get_ratings, get_reviews, get_breakdown, get_recipes
+from Backend.model2 import model1, model2, final_model, get_users, get_name, get_ratings, get_reviews, get_breakdown, get_recipes
 
 # Function to get or create the session state
 def get_session_state():
@@ -60,14 +59,6 @@ def user_profile():
     reviews_n = st.selectbox("Retrive number of past reviews:", [5,10,15])
     st.table(get_reviews(session_state.selected_author_id, reviews_n))
 
-def hybrid_food_recommender():
-    # Add content for the hybrid food recommender page
-    session_state = get_session_state()
-
-    st.title("Hybrid Food Recommender")
-    st.subheader('Step into Your Exclusive and Extraordinary Food Adventure!')
-    final_n = st.slider("Top N Recommendations", min_value=5, max_value=20, value=10)
-
 def algorithm():
     # Add content for the algorithm page
     session_state = get_session_state()
@@ -88,7 +79,35 @@ def algorithm():
                 word2vec_result = model2(session_state.selected_author_id)
                 st.success("Recommendations generated successfully!")
                 st.write(f"Top {model_n} Recommended Food Items for User {session_state.selected_author_id} Using Word2vec Content-Based Filtering:")
-                st.table(word2vec_result.head(model_n))
+                word2vec_result = word2vec_result.head(model_n)
+                st.table(word2vec_result)
+                word2vec_result_full = get_recipes(word2vec_result)
+
+                # Display detailed information for each recommended item
+                for index, row in word2vec_result.iterrows():
+                    recipe_id = row['RecipeId']
+                    word2vec_details = word2vec_result_full[word2vec_result_full['RecipeId'] == recipe_id]
+                    expander = st.expander(f"{row['Name']} (Recipe ID: {recipe_id})")
+
+                    # Display details in the expander
+                    # Display image using the provided link
+                    image_link = word2vec_details['Images'].iloc[0]
+                    if pd.notna(image_link):  # Check if the value is not NaN
+                        expander.image(image_link, caption=row['Name'], use_column_width=True)
+                    expander.write(f"Food Category: {word2vec_details['RecipeCategory'].iloc[0]}")
+
+                    # Display nutritional information in a table
+                    expander.write("### Nutritional Information:")
+                    word2vec_nutritional_info = word2vec_details[['Calories', 'FatContent', 'SaturatedFatContent', 'CholesterolContent',
+                                                'SodiumContent', 'CarbohydrateContent', 'FiberContent', 'SugarContent',
+                                                'ProteinContent']]
+                    expander.table(word2vec_nutritional_info)
+                    expander.write(f"Description: {word2vec_details['Description'].iloc[0]}")
+
+                    expander.write("### Recipe Information:")
+                    word2vec_duration_info = word2vec_details[['CookTime','PrepTime','TotalTime']]
+                    expander.table(word2vec_duration_info)
+
             except Exception as e:
                 st.error("Oops! Looks like this algorithm does't work. Please try again later!")
                 st.error(f"An error occurred: {str(e)}")
@@ -96,9 +115,9 @@ def algorithm():
     if model == 'Collaborative Filtering':
         if st.button("Recommend"):
             try:
-                # with st.spinner('Starting Your Personalized Food Recommendations...'):
                 svd_result = model1(session_state.selected_author_id)
                 st.write(f"Top {model_n} Recommended Food Items for User {session_state.selected_author_id} Using SVD Collaborative Filtering:")
+                st.success("Recommendations generated successfully!")
                 svd_result = svd_result.head(model_n)
                 st.table(svd_result)
                 svd_result_full = get_recipes(svd_result)
@@ -106,44 +125,50 @@ def algorithm():
                 # Display detailed information for each recommended item
                 for index, row in svd_result.iterrows():
                     recipe_id = row['RecipeId']
-                    details = svd_result_full[svd_result_full['RecipeId'] == recipe_id]
-                    expander = st.expander(f"{row['RecipeName']} (Recipe ID: {recipe_id})")
+                    svd_details = svd_result_full[svd_result_full['RecipeId'] == recipe_id]
+                    expander = st.expander(f"{row['Name']} (Recipe ID: {recipe_id})")
 
                     # Display details in the expander
                     # Display image using the provided link
-                    image_link = details['Images'].iloc[0]
-                    if image_link:
-                        expander.image(image_link, caption=row['RecipeName'], use_column_width=True)
-                        expander.write(f"Recipe Category: {details['RecipeCategory'].iloc[0]}")
+                    image_link = svd_details['Images'].iloc[0]
+                    if pd.notna(image_link):  # Check if the value is not NaN
+                        expander.image(image_link, caption=row['Name'], use_column_width=True)
+                    expander.write(f"Food Category: {svd_details['RecipeCategory'].iloc[0]}")
 
                     # Display nutritional information in a table
                     expander.write("### Nutritional Information:")
-                    nutritional_info = details[['Calories', 'FatContent', 'SaturatedFatContent', 'CholesterolContent',
+                    svd_nutritional_info = svd_details[['Calories', 'FatContent', 'SaturatedFatContent', 'CholesterolContent',
                                                 'SodiumContent', 'CarbohydrateContent', 'FiberContent', 'SugarContent',
                                                 'ProteinContent']]
-                    expander.table(nutritional_info)
-                    expander.write(f"Description: {details['Description'].iloc[0]}")
+                    expander.table(svd_nutritional_info)
+                    expander.write(f"Description: {svd_details['Description'].iloc[0]}")
 
                     expander.write("### Recipe Information:")
-                    duration_info = details[['CookTime','PrepTime','TotalTime']]
-                    expander.table(duration_info)
-
-                    # from PIL import Image
-                    # import requests
-                    # from io import BytesIO
-                    # # Assuming image_url contains the URL of the image
-                    # image_url = "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/12/30/3/picYyZVyR.jpg"
-
-                    # # Fetch the image
-                    # response = requests.get(image_url)
-                    # image = Image.open(BytesIO(response.content))
-
-                    # # Display the image in the expander
-                    # expander.image(image, caption=row['RecipeName'], use_column_width=True)
-
+                    svd_duration_info = svd_details[['CookTime','PrepTime','TotalTime']]
+                    expander.table(svd_duration_info)
             except Exception as e:
                 st.error("Oops! Looks like this algorithm does't work. Please try again later!")
                 st.error(f"An error occurred: {str(e)}")
+
+def hybrid_food_recommender():
+    # Add content for the hybrid food recommender page
+    session_state = get_session_state()
+
+    st.title("Hybrid Food Recommender")
+    st.subheader('Step into Your Exclusive and Extraordinary Food Adventure!')
+    final_n = st.slider("Top N Recommendations", min_value=5, max_value=20, value=10)
+
+    if st.button("Generate"):
+        try:
+            svd_result = model1(session_state.selected_author_id, 10)
+            word2vec_result = model2(session_state.selected_author_id, 10)
+            hybrid_result = final_model(session_state.selected_author_id, svd_result, word2vec_result)
+            st.success("Recommendations generated successfully!")
+            st.write(f"Top {final_n} Recommended Food Items for User {session_state.selected_author_id} Using Hybrid Approach:")
+            st.table(hybrid_result.head(final_n))
+        except Exception as e:
+            st.error("Oops! Looks like this algorithm does't work. Please try again later!")
+            st.error(f"An error occurred: {str(e)}")
 
 if page_selection == "Home Page":
     home_page()
